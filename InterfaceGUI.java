61b55616bd185ce4b2a1ba8e7d5c220cfa4c8a83
Calvin <calvinmoreiracosta@gmail.com>
2022-12-04 17:09:35 -0300

    Diversas mudanças realizadas

--- InterfaceGUI.java
+++ InterfaceGUI.java
@@ -1,13 +1,7 @@
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
-import javax.swing.event.ListSelectionEvent;
-import javax.swing.event.ListSelectionListener;
-import javax.swing.plaf.DimensionUIResource;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.*;
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 public class InterfaceGUI extends JFrame {
@@ -17,7 +11,8 @@ public class InterfaceGUI extends JFrame {
     Personagem persSelDir; // Personagem selecionado na lista direita
     String nomePersonagemSelEsq; // Nome do Personagem selecionado na lista esquerda
     String nomePersonagemSelDir; // Nome do Personagem selecionado na lista direita
-    JPanel painelPersonagensEsq = new JPanel(); // Painel que contém a lista esquerda para selecionar personagens
+    JPanel painelPersonagensEsq = new JPanel(); // Painel que contém a
+    // lista esquerda para selecionar personagens
     JLabel selPersJLabelEsq = new JLabel("Selecione o Personagem");
     JPanel painelPersonagensDir = new JPanel(); // Painel que contém a lista direita para selecionar personagens
     JLabel selPersJLabelDir = new JLabel("Selecione o Personagem");
@@ -36,6 +31,8 @@ public class InterfaceGUI extends JFrame {
     JLabel peDir = new JLabel("PE:");
     JPanel painelBotoes = new JPanel(); // Painel que contém os botões da interface
     JPanel painelConfronto = new JPanel(); // Painel que contém a descrição do confronto
+    JPanel bordaPreta = new JPanel(); // Borda pra dividir os dois paineis de informações de personagem
+    JPanel painelBranco = new JPanel(); // Painel para adicionar uma margente entre a borda e o próximo painel
 
     JTextArea descCombate = new JTextArea();
     JScrollPane scrlPane = new JScrollPane(descCombate);
@@ -43,56 +40,84 @@ public class InterfaceGUI extends JFrame {
     JButton botaoIniciaConfronto = new JButton("Começar Combate");
     JButton botaoResetaConfronto = new JButton("Resetar Personagens");
     ImageIcon img = new ImageIcon("images\\heroicon.png");
+    // cores da GUI
+    Color corFundoListas = new Color(95, 157, 247);
+    Color corFundoPaineis = new Color(23, 70, 162);
+    Color corFundoPaineisBotoes = new Color(255, 29, 29);
+    Color corPainelInfos = new Color(255, 247, 233);
 
     // Construtor
-    public InterfaceGUI(List<Personagem> listaPersonagens) {
-        this.listaDePersonagens = listaPersonagens;
-        setListaNomeIgualANomePersonagens(listaPersonagens);
+    public InterfaceGUI(
+            List<Personagem> listaPersonagens) {
+
+        // Setando dados básicos do programa
         this.setBounds(0, 0, 1024, 720);
         this.setTitle("A3 Super Heróis!");
         this.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         this.setIconImage(img.getImage());
         this.setLayout(new GridBagLayout());
-        setButtonStyleOne(botaoIniciaConfronto);
-        setButtonStyleOne(botaoResetaConfronto);
+        this.setBackground(Color.black);
+        // Fazendo um tratamento das listas
+        this.listaDePersonagens = listaPersonagens;
+        setListaNomeIgualANomePersonagens(listaPersonagens);
         JList listaPersonagemEsq = new JList(listaNomesDosPersonagens.toArray());
         JList listaPersonagemDir = new JList(listaNomesDosPersonagens.toArray());
         setEstiloDasListas(listaPersonagemEsq);
         setEstiloDasListas(listaPersonagemDir);
-        listaPersonagemEsq.setBorder(new EmptyBorder(15, 5, 15, 5));
+        // Adicionando os listeners das listas
         listaPersonagemEsq.addListSelectionListener(e -> {
             persSelEsq = achaPersDeNomeIgualA((String) listaPersonagemEsq.getSelectedValue());
             setCharInfos(0);
         });
-
-        listaPersonagemDir.setFixedCellHeight(30);
-        listaPersonagemDir.setFixedCellWidth(120);
-        listaPersonagemDir.setBorder(new EmptyBorder(30, 0, 30, 0));
         listaPersonagemDir.addListSelectionListener(e -> {
             persSelDir = achaPersDeNomeIgualA((String) listaPersonagemDir.getSelectedValue());
             setCharInfos(1);
         });
+
+        // Setando estilo dos botões
+        setEstiloBotao(botaoIniciaConfronto);
+        setEstiloBotao(botaoResetaConfronto);
+        setInstrucoes();
+        // Configurando o Layout manager(GroupLayout) interno do painel com a lista
+        // esquerda de personagens
         GroupLayout gLaySelEsq = new GroupLayout(painelPersonagensEsq);
         painelPersonagensEsq.setLayout(gLaySelEsq);
         gLaySelEsq.setHorizontalGroup(
                 gLaySelEsq.createSequentialGroup()
+                        .addGap(10, 20, 20)
                         .addGroup(gLaySelEsq.createParallelGroup(GroupLayout.Alignment.TRAILING)
                                 .addComponent(selPersJLabelEsq).addComponent(listaPersonagemEsq)));
 
         gLaySelEsq.setVerticalGroup(gLaySelEsq.createSequentialGroup()
+                .addGap(10, 20, 20)
                 .addGroup(gLaySelEsq.createParallelGroup(GroupLayout.Alignment.CENTER)
                         .addComponent(selPersJLabelEsq))
+                .addGap(10, 10, 10)
                 .addGroup(gLaySelEsq.createParallelGroup(GroupLayout.Alignment.CENTER)
                         .addComponent(listaPersonagemEsq)));
 
+        // Configurando o Layout manager(GroupLayout) interno do painel com a lista
+        // direita de personagens
+        GroupLayout gLaySelDir = new GroupLayout(painelPersonagensDir);
+        painelPersonagensDir.setLayout(gLaySelDir);
+        gLaySelDir.setHorizontalGroup(
+                gLaySelDir.createSequentialGroup()
+                        .addGap(10, 20, 20)
+                        .addGroup(gLaySelDir.createParallelGroup(GroupLayout.Alignment.TRAILING)
+                                .addComponent(selPersJLabelDir).addComponent(listaPersonagemDir)));
+
+        gLaySelDir.setVerticalGroup(gLaySelDir.createSequentialGroup()
+                .addGap(10, 20, 20)
+                .addGroup(gLaySelDir.createParallelGroup(GroupLayout.Alignment.CENTER)
+                        .addComponent(selPersJLabelDir))
+                .addGap(10, 10, 10)
+                .addGroup(gLaySelDir.createParallelGroup(GroupLayout.Alignment.CENTER)
+                        .addComponent(listaPersonagemDir)));
+
         selPersJLabelEsq.setForeground(Color.white);
-
-        GroupLayout gLayBotoes = new GroupLayout(painelBotoes);
-        gLayBotoes.setHorizontalGroup(gLayBotoes.createSequentialGroup().addComponent(botaoIniciaConfronto)
-                .addComponent(botaoResetaConfronto));
-        gLayBotoes.setVerticalGroup(gLayBotoes.createSequentialGroup().addComponent(botaoIniciaConfronto)
-                .addComponent(botaoResetaConfronto));
-
+        selPersJLabelDir.setForeground(Color.white);
+        // Configurando o Layout manager(GroupLayout) interno do painel esquerdo de
+        // informações do personagem selecionado na lista esquerda
         GroupLayout gLayInfoEsq = new GroupLayout(painelInfosEsq);
         painelInfosEsq.setLayout(gLayInfoEsq);
         gLayInfoEsq.setHorizontalGroup(
@@ -115,6 +140,10 @@ public class InterfaceGUI extends JFrame {
                                 .addComponent(peEsq))
                         .addGroup(gLayInfoEsq.createParallelGroup(GroupLayout.Alignment.BASELINE)
                                 .addComponent(fotoEsq)));
+
+        // Configurando o Layout manager(GroupLayout) interno do painel direito de
+        // informações do personagem selecionado na lista direita
+
         GroupLayout gLayInfoDir = new GroupLayout(painelInfosDir);
         painelInfosDir.setLayout(gLayInfoDir);
         gLayInfoDir.setHorizontalGroup(
@@ -138,22 +167,41 @@ public class InterfaceGUI extends JFrame {
                         .addGroup(gLayInfoDir.createParallelGroup(GroupLayout.Alignment.BASELINE)
                                 .addComponent(fotoDir)));
 
+        // Configurando o Layout manager(GroupLayout) interno dos botões
+        GroupLayout gLayBotoes = new GroupLayout(painelBotoes);
+        gLayBotoes.setHorizontalGroup(gLayBotoes.createSequentialGroup().addComponent(botaoIniciaConfronto)
+                .addComponent(botaoResetaConfronto));
+        gLayBotoes.setVerticalGroup(gLayBotoes.createSequentialGroup().addComponent(botaoIniciaConfronto)
+                .addComponent(botaoResetaConfronto));
+
+        // Adicionando o painel que contém a lista esquerda de personagens ao
+        // GridBagLayout
+
         addConstraints(this, painelPersonagensEsq, 0, 0, 1, 1, GridBagConstraints.BOTH, 0.1f, 0);
-        painelPersonagensEsq.setBackground(new Color(64, 73, 202));
-        addConstraints(this, painelInfos, 1, 0, 1, 1, GridBagConstraints.BOTH);
-        painelInfos.setBackground(Color.white);
+        painelPersonagensEsq.setBackground(corFundoPaineis);
+
+        // Adicionando o painel de informações dos personagens ao GridBagLayout
+        addConstraints(this, painelInfos, 1, 0, 1, 1, GridBagConstraints.BOTH, new Insets(10, 10, 10, 10));
+        painelInfos.setBackground(corPainelInfos);
         painelInfos.add(painelInfosEsq);
         painelInfosEsq.setPreferredSize(new Dimension(225, 315));
-        painelInfosEsq.setBackground(Color.white);
+        bordaPreta.setBackground(corFundoPaineis);
+        bordaPreta.setPreferredSize(new Dimension(1, 335));
+        painelInfos.add(bordaPreta);
+        painelBranco.setBackground(corPainelInfos);
+        ;
+        painelBranco.setPreferredSize(new Dimension(50, 335));
+        painelInfos.add(painelBranco);
+        painelInfosEsq.setBackground(corPainelInfos);
         painelInfos.add(painelInfosDir);
         painelInfosDir.setPreferredSize(new Dimension(225, 315));
-        painelInfosDir.setBackground(Color.white);
+        painelInfosDir.setBackground(corPainelInfos);
 
         addConstraints(this, painelPersonagensDir, 2, 0, 1, 1, GridBagConstraints.BOTH, 0.1f, 0);
         painelPersonagensDir.add(new JLabel("Teste 3"));
 
         painelPersonagensDir.add(listaPersonagemDir);
-        painelPersonagensDir.setBackground(new Color(64, 73, 202));
+        painelPersonagensDir.setBackground(corFundoPaineis);
 
         botaoIniciaConfronto.setSize(512, 30);
         botaoIniciaConfronto.addActionListener(w -> {
@@ -166,8 +214,7 @@ public class InterfaceGUI extends JFrame {
                 }
                 setCharInfos(2);
             } catch (Exception e) {
-                e.printStackTrace();
-                System.out.println(e);
+                descCombate.append("Um ou mais personagem não foi selecionado. Tente novamente!");
             }
         });
 
@@ -176,20 +223,23 @@ public class InterfaceGUI extends JFrame {
             try {
                 achaPersDeNomeIgualA("Parasita").setPeInicial(70);
                 setCharInfos(2);
+                // descCombate.append("Energia do personagem Parasita resetada!");
             } catch (Exception w) {
                 System.out.println("Algum erro ocorreu");
             }
         });
-        addConstraints(this, painelBotoes, 0, 1, 3, 1, GridBagConstraints.BOTH, 0.1f, 0);
-        addConstraints(this, painelConfronto, 0, 2, 3, 3, GridBagConstraints.BOTH, 0, 5);
+        addConstraints(this, painelBotoes, 0, 2, 3, 1, GridBagConstraints.BOTH, 0.1f, 0);
+        addConstraints(this, painelConfronto, 0, 3, 3, 3, GridBagConstraints.BOTH, 0, 5);
 
         painelConfronto.setLayout(new BorderLayout());
         painelConfronto.add(scrlPane);
+
+        painelBotoes.setBackground(corFundoPaineisBotoes);
         descCombate.setOpaque(false);
         descCombate.setForeground(Color.white);
         scrlPane.getViewport().setOpaque(false);
         scrlPane.setOpaque(false);
-        painelConfronto.setBackground(Color.darkGray);
+        painelConfronto.setBackground(corFundoPaineis);
 
         this.setVisible(true);
     }
@@ -238,11 +288,17 @@ public class InterfaceGUI extends JFrame {
 
     }
 
-    // Metodo para estilizar os botões
-    public void setButtonStyleOne(JButton button) {
-        button.setFocusable(false);
-        button.setBackground(new Color(61, 181, 210));
-        button.setForeground(new Color(185, 80, 0));
+    // Metodo addConstraints sobrecarregado com peso e insets
+    public void addConstraints(Container container, Component component, int gridx, int gridy, int width, int height,
+            int fill, float weightx, float weighty, Insets insets) {
+        GridBagConstraints gbc = new GridBagConstraints();
+        gbc.gridx = gridx;
+        gbc.gridy = gridy;
+        gbc.gridwidth = width;
+        gbc.gridheight = height;
+        gbc.fill = fill;
+        gbc.insets = insets;
+        container.add(component, gbc);
 
     }
 
@@ -287,13 +343,26 @@ public class InterfaceGUI extends JFrame {
         }
     }
 
+    // Metodo para estilizar os botões
+    public void setEstiloBotao(JButton button) {
+        button.setFocusable(false);
+        button.setBackground(corFundoPaineis);
+        button.setForeground(Color.white);
+
+    }
+
     public void setEstiloDasListas(JList list) {
         list.setFixedCellHeight(30);
         list.setFixedCellWidth(120);
-        list.setBackground(Color.darkGray);
-        list.setForeground(Color.white);
+        list.setBackground(corFundoListas);
+        list.setForeground(Color.black);
         list.setSelectionForeground(Color.white);
-        list.setSelectionBackground(Color.darkGray);
+        list.setSelectionBackground(corFundoPaineisBotoes);
         list.setBorder(new EmptyBorder(15, 5, 15, 5));
+    }
+
+    public void setInstrucoes() {
+        descCombate.append(String.format(
+                "Bem vindo ao Projeto Confronto entre SuperHeróis e SuperVilões!%n%n%nEste projeto foi feito como trabalho da avaliação A3 da UC Programação de Soluções Computacionais.%n%nSua utilização é bem simples:%n%n1. Selecione um personagem na lista esquerda ou direita.%n2. Selecione um personagem na outra lista.%n3. Clique no botão 'Começar Combate'.%n4. Caso desejar, pode mudar os personagens escolhidos em alguma lista e iniciar um novo combate.%n%nInformações importantes:%n(a) Os Personagens 'Mímico' e 'Superskrull' copiam os poderes do personagem adversário.%n(b) Caso o Personagem 'Parasita' vença o duelo, ele irá drenar a energia final de seu oponente. %nPara resetar a energia dele, é só clicar no botão 'Resetar Personagens'.%nEspero que gostem do trabalho! "));
     }
 }
--- Main.java
+++ Main.java
@@ -12,24 +12,24 @@ public class Main {
                 Poder visaoSuperman = new Poder("Visão de Raios", 5, 5);
                 Poder soproSuperman = new Poder("Super Sopro", 4, 4);
                 Heroi superman = new Heroi(pronomeO, "Superman", dc, 100, 70, "Clark Kent",
-                                new ImageIcon("images\\superman_img.jpg"), fSuperman, visaoSuperman,
+                                new ImageIcon("images\\superman.png"), fSuperman, visaoSuperman,
                                 soproSuperman);
                 listaPersonagens.add(superman);
 
                 Poder fHomemAranha = new Poder("Força", 7, 5);
                 Poder soltarTeia = new Poder("Soltar Teia", 5, 5);
                 Heroi homemAranha = new Heroi(pronomeO, "Homem-Aranha", marvel, 80, 50, "Peter Parker",
-                                new ImageIcon("images\\homenaranha_img.jpg"), fHomemAranha,
+                                new ImageIcon("images\\homenaranha.png"), fHomemAranha,
                                 soltarTeia);
                 listaPersonagens.add(homemAranha);
 
                 Poder fFenix = new Poder("Força", 12, 10);
                 Heroi fenix = new Heroi(pronomeA, "Fênix", marvel, 150, 100, "Jean Grey",
-                                new ImageIcon("images\\fenix_img.jpg"), fFenix);
+                                new ImageIcon("images\\fenix.png"), fFenix);
                 listaPersonagens.add(fenix);
 
                 Heroi mimico = new Heroi(pronomeO, "Mímico", marvel, 70, 50, "Calvin Montgomery Rankin",
-                                new ImageIcon("images\\mimico_img.jpg"));
+                                new ImageIcon("images\\mimico.png"));
                 mimico.setTipoMimico(true);
                 listaPersonagens.add(mimico);
 
@@ -39,30 +39,30 @@ public class Main {
                 Poder visaoBizarro = new Poder("Visao de Raios", 5, 5);
                 Poder soproBizarro = new Poder("Supersopro", 4, 4);
                 Vilao superBizarro = new Vilao(pronomeO, "Superbizarro", dc, 95, 65, 50,
-                                new ImageIcon("images\\superbizarro_img.jpg"), fSuperBizarro,
+                                new ImageIcon("images\\superbizarro.png"), fSuperBizarro,
                                 soproBizarro, visaoBizarro);
                 listaPersonagens.add(superBizarro);
 
                 Poder fDuende = new Poder("Força", 7, 5);
                 Poder granada = new Poder("Granada de Abóbora", 5, 5);
                 Vilao duendeVerde = new Vilao(pronomeO, "Duende Verde", marvel, 80, 60, 70,
-                                new ImageIcon("images\\duendeverde_img.jpg"), fDuende, granada);
+                                new ImageIcon("images\\duendeverde.png"), fDuende, granada);
                 listaPersonagens.add(duendeVerde);
 
                 Poder tentaculos = new Poder("Tentáculos", 6, 4);
                 Vilao doutorOctopus = new Vilao(pronomeO, "Doutor Octopus", marvel, 70, 40, 40,
-                                new ImageIcon("images\\droctopus_img.jpg"), tentaculos);
+                                new ImageIcon("images\\droctopus.png"), tentaculos);
                 listaPersonagens.add(doutorOctopus);
 
                 Vilao superSkrull = new Vilao(pronomeO, "SuperSkrull", marvel, 90, 60, 100,
-                                new ImageIcon("images\\superskrull_img.jpg"));
+                                new ImageIcon("images\\superskrull.png"));
                 superSkrull.setTipoMimico(true);
                 listaPersonagens.add(superSkrull);
 
                 Poder fParasita = new Poder("Força", 7, 8);
                 Poder rajada = new Poder("Rajada Energética", 6, 8);
                 Vilao parasita = new Vilao(pronomeO, "Parasita", dc, 90, 70, 70,
-                                new ImageIcon("images\\parasita_img.jpg"), fParasita, rajada);
+                                new ImageIcon("images\\parasita.png"), fParasita, rajada);
                 parasita.setTipoParasita(true);
                 listaPersonagens.add(parasita);
                 InterfaceGUI guii = new InterfaceGUI(listaPersonagens);
